4defa9558fc71dbf2b9e3ef3ffe3dbe1f9fff69f
Stan125 <stanislaus@stadlmann.cm>
2016-05-27 16:22:59 +0200

    fix very annoying windows backslash bug

--- R/wd_check.R
+++ R/wd_check.R
@@ -6,15 +6,23 @@
 #' wd_check("/Users/stani/GitHub/GREA/data/csv_food_supply.csv")
 
 wd_check <- function(filelocation) {
-  file_split <- strsplit(filelocation, "/")[[1]]
-  wd_split <- strsplit(getwd(), "/")[[1]]
+  # deconstruct file path depending on platform
+  if (grepl("[\\]", filelocation)) {
+    file_split <- unlist(strsplit(filelocation, "[\\]"))
+    wd_split <- unlist(strsplit(getwd(), "[\\]"))
+  } else if (grepl("/", filelocation)) {
+    file_split <- unlist(strsplit(filelocation, "/"))
+    wd_split <- unlist(strsplit(getwd(), "/"))
+  }
 
   # Return filepaths depending on whether the file is in the WD
   if (all(wd_split %in% file_split)) {
     # Return filepath without WD
-    file_in_wd <- paste(file_split[seq(length(wd_split) + 1, length(file_split))], collapse = "/")
+    file_in_wd <- file_split[seq(length(wd_split) + 1, length(file_split))]
+    file_in_wd <- do.call("file.path", as.list(file_in_wd))
     return(file_in_wd)
   } else {
-    return(filelocation)
+    file <- do.call("file.path", as.list(file_split))
+    return(file)
   }
 }
